607b6317144e91718c6909dfa336ccd2b2837ed0
adammartin <ebondark.od@gmail.com>
2016-08-08 10:39:31 -0500

    First stage complete

--- primes/prime_factors.py
+++ primes/prime_factors.py
@@ -1,4 +1,20 @@
+import math
 class Primes(object):
 
 	def factors(self, number):
-		return None
+		if number > 1:
+			upperBound = int(round(math.sqrt(number))+1)
+			for divisor in range(2,upperBound):
+				if number % divisor == 0:
+					return self.flatten([divisor, self.factors(number/divisor)])
+			return [number]
+		return []
+
+	def flatten(self, list):
+		new_list = []
+		for item in list:
+			if type(item) == type([]):
+				new_list.extend(self.flatten(item))
+			else:
+				new_list.append(item)
+		return new_list
--- test/test_prime_factors.py
+++ test/test_prime_factors.py
@@ -7,5 +7,30 @@ class PrimeTests(unittest.TestCase):
 	def setUp(self):
 		self.primes = Primes()
 
-	def test_primes_of_0_is_an_empty_list(self):
-		nt.assert_equal(True, False)
+	def test_primes_of_1_is_an_empty_list(self):
+		self.assertEqual([],self.primes.factors(1))
+
+	def test_primes_of_2_returns_2(self):
+		self.assertEqual([2],self.primes.factors(2))
+
+	def test_primes_of_3_returns_3(self):
+		self.assertEqual([3],self.primes.factors(3))
+
+	def test_primes_of_4_returns_2_2(self):
+		self.assertEqual([2,2],self.primes.factors(4))
+
+	def test_primes_of_6_returns_2_3(self):
+		self.assertEqual([2,3],self.primes.factors(6))
+
+	def test_primes_of_8_returns_2_2_2(self):
+		self.assertEqual([2,2,2], self.primes.factors(8))
+
+	def test_primes_of_9_returns_3_3(self):
+		self.assertEqual([3,3], self.primes.factors(9))
+
+class FlattenTest(unittest.TestCase):
+	def test_flatten_for_doublenest_flattens(self):
+		self.assertEqual([2], Primes().flatten([[2]]) )
+
+	def test_flatten_for_random_nesting(self):
+		self.assertEqual([2,2], Primes().flatten([2,[2]]))
