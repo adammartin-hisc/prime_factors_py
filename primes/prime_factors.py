850d79e0881eace08f1203255726737650fbfe3d
adammartin <ebondark.od@gmail.com>
2016-08-08 14:32:09 -0500

    Completed with refactoring

--- primes/prime_factors.py
+++ primes/prime_factors.py
@@ -1,4 +1,11 @@
 class Primes(object):
 
 	def factors(self, number):
-		return None
+		factors = []
+		current_factor = 2
+		while (number > 1):
+			while (number % current_factor == 0):
+				factors.append(current_factor)
+				number /= current_factor
+			current_factor += 1
+		return factors
--- test/test_prime_factors.py
+++ test/test_prime_factors.py
@@ -8,4 +8,20 @@ class PrimeTests(unittest.TestCase):
 		self.primes = Primes()
 
 	def test_primes_of_0_is_an_empty_list(self):
-		nt.assert_equal(True, False)
+		nt.assert_equal(self.primes.factors(0), [])
+
+	def test_primes_of_2_is_2(self):
+		nt.assert_equal(self.primes.factors(2), [2])
+
+	def test_primes_of_powers2_is_2s(self):
+		nt.assert_equal(self.primes.factors(4), [2, 2])
+		nt.assert_equal(self.primes.factors(8), [2, 2, 2])
+
+	def test_primes_of_3_is_3(self):
+		nt.assert_equal(self.primes.factors(3), [3])
+
+	def test_primes_of_6_is_2_3(self):
+		nt.assert_equal(self.primes.factors(6), [2, 3])
+
+	def test_primes_of_35_is_5_7(self):
+		nt.assert_equal(self.primes.factors(35), [5, 7])
