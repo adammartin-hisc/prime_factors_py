f510ed1605549b2df3d6a79a40ca77193d6aae14
adammartin <ebondark.od@gmail.com>
2016-08-16 14:25:43 -0500

    Couldn't finish ... got frustrated but was on cusp of awesome

--- primes/prime_factors.py
+++ primes/prime_factors.py
@@ -1,4 +1,8 @@
 class Primes(object):
 
 	def factors(self, number):
-		return None
+		nums = []
+		for n in range(2, number+1):
+			if number % n == 0:
+				nums.append(n)
+		return nums
--- test/test_prime_factors.py
+++ test/test_prime_factors.py
@@ -7,5 +7,19 @@ class PrimeTests(unittest.TestCase):
 	def setUp(self):
 		self.primes = Primes()
 
-	def test_primes_of_0_is_an_empty_list(self):
-		nt.assert_equal(True, False)
+	def test_primes_is_an_empty_list(self):
+		result = self.primes.factors(0)
+		nt.assert_equal(result, [])
+
+	def test_primes_has_results(self):
+		result = self.primes.factors(2)
+		nt.assert_equal(len(result), 1)
+		nt.assert_equal(result[0], 2)
+
+	def test_primes_has_results_for_three(self):
+		result = self.primes.factors(3)
+		nt.assert_equal(result, [3])
+
+	def test_primes_has_results_for_four(self):
+		result = self.primes.factors(4)
+		nt.assert_equal(result, [2, 2])
